f626743aef2015a14bdff2576d59a430e4a29285
Kyyuserbot <83812990+Rifkiarisman@users.noreply.github.com>
2021-07-25 21:42:39 +0700

    Update admins.py

--- VCsMusicBot/modules/admins.py
+++ VCsMusicBot/modules/admins.py
@@ -24,7 +24,7 @@ async def update_admin(client, message: Message):
             for member in await message.chat.get_members(filter="administrators")
         ],
     )
-    await message.reply_text("❇️ Admin cache refreshed!")
+    await message.reply_text("🔥 Admin cache refreshed!")
 
 
 @Client.on_message(command("pause") & other_filters)
@@ -35,7 +35,7 @@ async def pause(_, message: Message):
     if (chat_id not in callsmusic.active_chats) or (
         callsmusic.active_chats[chat_id] == "paused"
     ):
-        await message.reply_text("❗ Nothing is playing!")
+        await message.reply_text("❗ Tidak Ada Musik Yang DiMainkan!")
     else:
         callsmusic.pause(chat_id)
         await message.reply_text("▶️ Paused!")
@@ -49,7 +49,7 @@ async def resume(_, message: Message):
     if (chat_id not in callsmusic.active_chats) or (
         callsmusic.active_chats[chat_id] == "playing"
     ):
-        await message.reply_text("❗ Nothing is paused!")
+        await message.reply_text("❗ Tidak Ada Yang Bisa Di Pause!")
     else:
         callsmusic.resume(chat_id)
         await message.reply_text("⏸ Resumed!")
@@ -61,7 +61,7 @@ async def resume(_, message: Message):
 async def stop(_, message: Message):
     chat_id = get_chat_id(message.chat)
     if chat_id not in callsmusic.active_chats:
-        await message.reply_text("❗ Nothing is streaming!")
+        await message.reply_text("❗ Tidak Ada Musik Yang Berjalan!")
     else:
         try:
             queues.clear(chat_id)
@@ -69,7 +69,7 @@ async def stop(_, message: Message):
             pass
 
         await callsmusic.stop(chat_id)
-        await message.reply_text("❌ Stopped streaming!")
+        await message.reply_text("❌ Memberhentikan Musik!")
 
 
 @Client.on_message(command("skip") & other_filters)
@@ -79,7 +79,7 @@ async def skip(_, message: Message):
     global que
     chat_id = get_chat_id(message.chat)
     if chat_id not in callsmusic.active_chats:
-        await message.reply_text("❗ Nothing is playing to skip!")
+        await message.reply_text("❗ Tidak Ada Yang Bisa Di Skip!")
     else:
         queues.task_done(chat_id)
         if queues.is_empty(chat_id):
@@ -108,4 +108,4 @@ async def admincache(client, message: Message):
             for member in await message.chat.get_members(filter="administrators")
         ],
     )
-    await message.reply_text("❇️ Admin cache refreshed!")
+    await message.reply_text("🔥 Admin cache refreshed!\n\n🔥Bot Restarted")
